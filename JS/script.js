6f0054975dfa54cca63caf963f673d1de1c1a839
Jomerjom <ekenesjoachim@gmail.com>
2023-12-07 14:05:56 +0100

    Added custom error message if no results found

--- JS/script.js
+++ JS/script.js
@@ -9,10 +9,10 @@ const traits = [
     "Morsom", "Aktiv", "Rolig", "Selvsikker", "Nervøs"
 ]
 
-// lager en tom array for å lagre dyrene
+// lager et tomt array for å lagre dyrene
 let pets = []
 
-// lager en funksjon som genererer et tilfeldig tall mellom min og max
+// lager en funksjon som genererer et tilfeldig tall med gitt min og max verdier
 const randomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 // lager en funksjon som genererer et tilfeldig tall mellom 0 og lengden til arrayen
@@ -43,28 +43,44 @@ function generatePet() {
     }
 )}
 
-// kaller på funksjonen
-generatePet()
-
 // lager en funksjon som finner det første dyret
-const findAnimal = (animal) => pets.find(pet => pet.animal === animal)
+const findAnimal = (animal) => {
+    const result = pets.find(pet => pet.animal === animal);
+    return result ? result : "No results found for given animal";
+}
 
 // lager en funksjon som finner det første dyret med en gitt farge
-const findFirstIndexByColor = (color) => pets.findIndex(pet => pet.color === color)
-
+const findFirstIndexByColor = (color) => {
+    const result = pets.findIndex(pet => pet.color === color);
+    return result !== -1 ? result : "No results found for given color";
+}
 
 // lager en funksjon som finner det siste dyret med en gitt alder
-// er vel ikke noe poeng og bruke både findLastIndex og findLast her?
-const findLastAnimalByAge = (age) => pets.findLast(pet => pet.age === age)
+const findLastAnimalByAge = (age) => {
+    const result = pets.reverse().find(pet => pet.age === age);
+    return result ? result : "No results found for given age";
+}
 
 // lager en funksjon som finner alle dyrene med en gitt egenskap
-const filterByTraits = (traits) => pets.filter(pet => pet.traits.includes(traits))
+const filterByTraits = (traits) => {
+    const result = pets.filter(pet => pet.traits.includes(traits));
+    return result.length > 0 ? result : "No results found for given traits";
+}
 
 // lager en funksjon som console logger alle dyrene og deres egenskaper
 const consoleLogAllPets = () => {
     pets.map(pet => {
-        console.log(`${pet.animal}en er ${pet.age} år gammel, ${pet.color} og har egenskapene ${pet.traits.join(', ')}`)
-    })
+        console.log(`${pet.animal}en er ${pet.age} år gammel, ${pet.color} og er ${pet.traits.join(', ')}`);
+    });
 }
 
-consoleLogAllPets()
+// kaller på funksjonene
+generatePet();
+console.log(pets);
+consoleLogAllPets();
+
+// tester alle find og filter funksjonene
+console.log(findAnimal("Katt"));
+console.log(findFirstIndexByColor("Blå"));
+console.log(findLastAnimalByAge(5));
+console.log(filterByTraits("Rolig"));
